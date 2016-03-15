4533256ea102149e6fd2ca87d1182e2bd9b55877
canapio <canapio.developer@gmail.com>
2016-03-15 19:51:58 +0900

    Roast Meister server 002

--- RoastMeisterServer002/server.js
+++ RoastMeisterServer002/server.js
@@ -1,1 +1,29 @@
-console.log("hi")
+var express      =   require("express");
+var app          =   express();
+var bodyParser   =   require("body-parser");
+
+
+var router       =   express.Router();
+
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended" : true}));
+
+router.get("/",function(req,res){
+  res.json({"error" : false,"message" : "Wooooooooooooooooooooooooooo"});
+});
+
+
+
+app.use('/',router);
+
+
+
+
+// site_vhosts.push(express.vhost('rm.canapio.com', app));
+// vhost = express().apply(this, site_vhosts);
+
+app.listen(8000);
+console.log("Listening to PORT 8000");
