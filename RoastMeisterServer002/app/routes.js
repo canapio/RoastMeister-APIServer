59ece36696abeeae8e2d30eec514e0b4759f626d
canapio <canapio.developer@gmail.com>
2016-03-16 21:43:31 +0900

    Support web

--- RoastMeisterServer002/app/routes.js
+++ RoastMeisterServer002/app/routes.js
@@ -1,13 +1,19 @@
 
 var express = require('express');
-
-
+var fs = require('fs');
+var sampledataobject = JSON.parse(fs.readFileSync('dist/sampledata/carlozdata.json', 'utf8'));
+// console.log(sampledataobject)
 
 module.exports = function(passport) {
   var app = express.Router();
 
   app.get('/', function(req, res) {
     res.render('index.ejs');
+  });
+  app.get('/graphtest', function(req, res) {
+    res.render('graphtest.ejs', {
+      sampledata : sampledataobject
+    });
   });
   // app.get("/",function(req,res){
   //   res.json({"error" : false,"message" : "This is my server"});
